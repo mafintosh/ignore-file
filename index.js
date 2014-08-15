a6f2602cff0670e5f86052b57b709df12aeb9cd2
Mathias Buus <mathiasbuus@gmail.com>
2014-08-15 17:50:16 +0200

    be more gitignore compliant

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 var minimatch = require('minimatch')
 var fs = require('fs')
 
+var toRegExp = function(pattern) {
+  var i = pattern.indexOf('/')
+
+  if (i === -1 || i === pattern.length-1) {
+    pattern = minimatch.makeRe(pattern, {dot:true}).toString()
+      .replace(/^\/\^/, '/(^|\\/)')
+      .replace(/\$\/$/, '($|\\/)/')
+    return new RegExp(pattern.slice(1,-1), 'i')
+  }
+
+  if (i === 0) pattern = pattern.slice(1)
+
+  return minimatch.makeRe(pattern, {dot:true, nocase:true})
+}
+
+var falsy = function() {
+  return false
+}
+
+var toFunction = function(regexp) {
+  return function(input) {
+    return regexp.test(input)
+  }
+}
+
+var or = function(a, b) {
+  if (a === falsy) return b
+  return function(input) {
+    return a(input) || b(input)
+  }
+}
+
 var ignore = function(filename, def, cb) {
   if (typeof def === 'function') return ignore(filename, null, def)
   fs.readFile(filename, 'utf-8', function(err, src) {
@@ -11,23 +43,38 @@ var ignore = function(filename, def, cb) {
 }
 
 ignore.compile = function(src) {
-  src = src.trim()
+  if (Array.isArray(src)) src = src.join('\n')
 
-  var lines = src ? src.split('\n') : []
-  if (!lines.length) return function() { return false }
-
-  return lines
+  var lines = src.split('\n')
     .map(function(line) {
-      var m = new minimatch.Minimatch(line.trim())
-      return function(filename) {
-        return m.match(filename)
-      }
+      return line.trim()
     })
-    .reduce(function(a, b) {
-      return function(filename) {
-        return a(filename) || b(filename)
-      }
+    .filter(function(line) {
+      return line && line[0] !== '#'
     })
+
+  var negative = lines
+    .filter(function(line) {
+      return line[0] !== '!'
+    })
+    .map(toRegExp)
+    .map(toFunction)
+    .reduce(or, falsy)
+
+  var positive = lines
+    .filter(function(line) {
+      return line[0] === '!'
+    })
+    .map(function(line) {
+      return line.slice(1)
+    })
+    .map(toRegExp)
+    .map(toFunction)
+    .reduce(or, falsy)
+
+  return function(input) {
+    return !positive(input) && negative(input)
+  }
 }
 
 ignore.sync = function(filename, def) {
